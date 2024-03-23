ffc63f6ec2fce77728e12782119108d3556055ec
Ryan Muehe <rmuehe@gmail.com>
2024-03-22 20:17:33 -0700

    Initial commit

---
+++ src/main/java/com/ryanmuehe/maintenancerecords/MaintenancerecordsApplication.java
@@ -0,0 +1,13 @@
+package com.ryanmuehe.maintenancerecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenancerecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenancerecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryanmuehe/maintenancerecords/MaintenancerecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanmuehe.maintenancerecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenancerecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
